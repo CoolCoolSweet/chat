1c8ad1194f40328204e8ee4986bd8416dcafb09b
Sean <38589300+CoolCoolSweet@users.noreply.github.com>
2018-05-21 16:39:55 -0400

    Room joining done, chat showing

--- dev/scripts/app.js
+++ dev/scripts/app.js
@@ -22,17 +22,23 @@ class App extends React.Component {
       userID: "",
       userRoom: "",
       userMessage: "",
+      createRoom: "",
+      roomList: [],
+      chatMessages: [],
     }
 
     this.handleInput = this.handleInput.bind(this);
     this.handleLogin = this.handleLogin.bind(this);
+    this.createRoom = this.createRoom.bind(this);
+    this.joinRoom = this.joinRoom.bind(this);
+    this.sendMessage = this.sendMessage.bind(this);
   }
   componentDidMount() {
     console.log("It's Alive!")
     // Set Firebase user persistence to NONE so that every reload/new page gets a new userID
     firebase.auth().setPersistence(firebase.auth.Auth.Persistence.NONE);
   }
-  
+
   // Methods (Are these called methods? ^_^)
   handleInput (e) {
     this.setState({
@@ -59,17 +65,81 @@ class App extends React.Component {
 
         // Set the onDisconnect to remove the user when they leave
         firebase.database().ref(`onlineUsers/${this.state.userID}`).onDisconnect().remove()
+
+        // Load roomList from server and convert to and array to setState
+        firebase.database().ref(`rooms`).on("value", (snapshot) => {
+          let tempArray = [];
+          for (let room in snapshot.val()) {
+            tempArray.push(room);
+          };
+          this.setState({
+            roomList: tempArray
+          });
+        })
       });
     }
   }
 
+  createRoom (e) {
+    e.preventDefault();
+    if (!(this.state.createRoom)) {
+      alert('Cannot create a room with no name!');
+    } else {
+      // Push the room to the FirebaseDB
+      firebase.database().ref(`rooms/${this.state.createRoom}`).set({
+        roomCreator: {
+          [this.state.userName]: this.state.userID
+        }
+      });
+      // Remove rooms created by the user on disconnect
+      firebase.database().ref(`rooms/${this.state.createRoom}`).onDisconnect().remove()
+    }
+  }
+
+  joinRoom (e) {
+    // setState to the room chosen to join
+    this.setState({
+      userRoom: e.target.getAttribute('name')
+    }, () => {
+      // pull chat for room from Firebase DB and setState of chatMessages
+      firebase.database().ref(`rooms/${this.state.userRoom}/messages`).on("value", (snapshot) => {
+        let tempArray = [];
+        for (let messageKey in snapshot.val()) {
+          tempArray.push(snapshot.val()[messageKey]);
+        };
+        this.setState({
+          chatMessages: tempArray
+        }, () => {
+          let scrollBot = document.getElementsByClassName("messageWindow")[0];
+          scrollBot.scrollTop = scrollBot.scrollHeight - scrollBot.clientHeight;
+        });  
+      })
+    });
+  }
+
+  sendMessage (e) {
+    e.preventDefault();
+    if (!(this.state.userRoom)) {
+      alert('Please join a room before sending a message!');
+    } else {
+      // Push the message to the room on Firebase DB
+      firebase.database().ref(`rooms/${this.state.userRoom}/messages`).push({
+        userName: this.state.userName,
+        userID: this.state.userID,
+        userMessage: this.state.userMessage
+      });
+    }
+  }
   render() {
     return (
       <div className="wrapper">
         <div className="chatArea">
           <ul className="messageWindow">
+            {this.state.chatMessages.map((message, i) => {
+              return <li className={message.userID === this.state.userID ? "chatMessage ownMessage" : "chatMessage"} key={message.userID + i}><p>{message.userMessage}</p></li>
+            })}
           </ul>
-          <form action="">
+          <form action="" onSubmit={this.sendMessage}>
             <input type="text" name="userMessage" value={this.state.userMessage} onChange={this.handleInput} className="messageInput" placeholder="Enter your message ..."/>
             <button type="submit" className="sendButton">Send</button>
           </form>
@@ -79,11 +149,16 @@ class App extends React.Component {
             <input type="text" name="userName" value={this.state.userName} onChange={this.handleInput} className={this.state.userID == "" ? "loginInput" : "loginInput loggedIn"} readOnly={this.state.userID == "" ? false : true} placeholder="Choose a name ..."/>
             <button className={this.state.userID == "" ? "loginButton" : "loginButton loggedInButton"} type="submit">Login</button>
           </form>
-          <form action="">
-            <input type="text" name="userRoom" value={this.state.userRoom} onChange={this.handleInput} className="roomInput" placeholder="Create a room ..."/>
+          <ul className="userList">
+          </ul>
+          <form action="" onSubmit={this.createRoom}>
+            <input type="text" name="createRoom" value={this.state.createRoom} onChange={this.handleInput} className="roomInput" placeholder="Create a room ..."/>
             <button className="roomButton" type="submit">Create</button>
           </form>
           <ul className="roomList">
+            {this.state.roomList.map((room, i) => {
+              return <li name={room} className={this.state.userRoom === room ? "selectedRoom" : ""} onClick={this.joinRoom} key={room + i}>{room}</li>
+            })}
           </ul>
         </div>
       </div>
